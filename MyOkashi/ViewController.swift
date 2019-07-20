77295af3ba5af70755c8f3039ab0f8345253b03c
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-20 21:03:11 +0900

    Create UI and API Requests

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Search Barのdalegate通知先を設定
+        searchText.delegate = self
+        // 入力のヒントになる、プレースホルダを設定
+        searchText.placeholder = "お菓子の名前を入力してください"
+        
     }
 
 
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 検索ボタンをクリック（タップ）時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // キーボードを閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            // デバッグエリアに出力
+            print(searchWord)
+            // 入力されていたら、お菓子を検索
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    
+    // searchOkashiメソッド
+    // 第一引数：keyword 検索したいワード
+    func searchOkashi(keyword : String) {
+        
+        // お菓子の検索キーワードをエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURLの組み立て
+        guard let req_url = URL(string: "http://www.sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
+    }
 }
 
